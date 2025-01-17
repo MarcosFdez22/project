4ec430c7c4c9453169000dcf21d4f961b229dd53
Marcos <a21marcosfv@iessanclemente.net>
2025-01-17 11:55:54 +0100

    a

--- book/models.py
+++ book/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Author(models.Model):
+    first_name= models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
 class Book(models.Model):
     title = models.CharField(max_length=50)
     raiting = models.IntegerField(
         validators= [MinValueValidator(0),MaxValueValidator(10)]
     )
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     bestSelling = models.BooleanField(default=False)
     def __str__(self):
         return f"{self.title} ({self.raiting})"
